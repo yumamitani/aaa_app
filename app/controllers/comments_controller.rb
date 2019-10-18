e0529251917ad2a450227dca4f094e68c362730b
yumamitani <ymsy.w.life3@icloud.com>
2019-10-18 10:41:32 +0900

    Update.comments_controller2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ def comment_params
 end
 
 def destroy
-  comment = comment.find(params[:id])
+  comment = Comment.find(params[:id])
   if comment.user_id == current_user.id
     comment.destroy
   end
